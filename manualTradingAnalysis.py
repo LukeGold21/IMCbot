9189e6940eed506900ff78e395d5257e320dbd2e
Luke Fiumara <lfiu3746@uni.sydney.edu.au>
2023-03-22 14:32:50 +1100

    finished manual trading analysis

--- manualTradingAnalysis.py
+++ manualTradingAnalysis.py
@@ -1,4 +1,5 @@
 currencies = ["PZ", "WR", "SB", "SC"]
+my_currency = "SC"
 
 exchange_rates = {"PZ":{"PZ":1, "WR":0.5, "SB":1.45, "SC":0.75},
                   "WR":{"PZ":1.95, "WR":1, "SB":3.1, "SC":1.49},
@@ -21,34 +22,41 @@ def permutation(lst):
     # Find the permutations for lst if there are
     # more than 1 characters
  
-    list = [] # empty list that will store current permutation
+    ls = [] # empty list that will store current permutation
  
     for i in lst:
         for j in lst:
             for k in lst:
                 for l in lst:
-                    list.append(["SC",i,j,k,l,"SC"])
+                    ls.append([my_currency,i,j,k,l,my_currency])
 
-    return list
+    return ls
 
 def process_perms(perms):
+    ok_perms = []
     for perm in perms:
-        if perm[-1] != "SC":
-            perms.remove(perm)
-    return perms
+        remove = False
+        for i in range(0, len(perm)-1):
+            if str(perm[i]) == str(perm[i+1]):
+                remove = True
+        if not remove:
+            ok_perms.append(perm)        
+    return ok_perms
 
 def calculate_exchange(trades, exchange_rate):
     trades_with_rate = []
     for trade in trades:
         rate = 1
         i=0
-        j=1
+        #print("\n" + str(trade), end = '')
         while i <len(trade)-1:
-            rate = rate*(exchange_rate.get(trade[i]).get(trade[j]))
-            trades_with_rate.append({trade:rate})
+            #print("  " + str(exchange_rate.get(trade[i]).get(trade[i + 1])), end = '')
+            rate = rate*(exchange_rate.get(trade[i]).get(trade[i+1]))
+            i += 1
+        trades_with_rate.append((trade, rate))
     return trades_with_rate
 
-currency_perms = calculate_exchange(permutation(currencies), exchange_rates)
+currency_perms = sorted((calculate_exchange(permutation(currencies), exchange_rates)), key=lambda x: x[1])
 
 for perm in currency_perms:
     print(perm)
